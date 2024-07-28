03cf6408baf2c3db9d3680a5466dcb53bcde3541
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2024-07-28 12:42:55 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-FOR /F %k in (requirements.txt) DO ( if NOT # == %k ( pip install %k ) )
+cat requirements.txt | xargs -n 1 pip install
 python -m scrape_and_process.scrapers
 python -m scrape_and_process.price_predict
