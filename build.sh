748134e29411c8b4d2b1e609a00b24c7f48d611c
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2024-07-28 12:57:56 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cat requirements.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 pip install
+cat requirements.txt | while read PACKAGE; do pip install "$PACKAGE"; done
 python -m scrape_and_process.scrapers
 python -m scrape_and_process.price_predict
