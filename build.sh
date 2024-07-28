0d307d3883ce4b07b110ec77b3796aa1d10154ba
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2024-07-28 12:41:49 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-pip install -r requirements.txt
-pip install requests
-pip install bs4
+FOR /F %k in (requirements.txt) DO ( if NOT # == %k ( pip install %k ) )
 python -m scrape_and_process.scrapers
 python -m scrape_and_process.price_predict
