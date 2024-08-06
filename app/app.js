974626834badd4ff33326b0cd49051c1d36788ba
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2024-08-06 10:28:47 -0400

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -9,9 +9,13 @@ app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 
 
+axios.get(api_link).then(res => {
+    console.log(res.data)
+})
 
 app.get('/', (req, res) => {
     axios.get(api_link).then(api_res => {
+        console.log(api_res.data)
         res.render("index", {data : JSON.stringify(api_res.data)})
     }).catch(err => {
         res.render("index", {data : {"Error" : err}})
@@ -19,8 +23,8 @@ app.get('/', (req, res) => {
 })
 
 cron.schedule('* * * * *', () => {
-  console.log('active');
-});
+    console.log('active');
+  });
 
 app.listen(5000, () => {
     console.log("Server listening on http://127.0.0.1:5000")
